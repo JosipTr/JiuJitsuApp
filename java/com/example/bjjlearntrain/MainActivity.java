519d4f11e044c66dd4e5d7f1a30a68cf0d93d1ce
JosipTr <josiptroha@gmail.com>
2021-08-29 17:57:37 +0200

    First commit

---
+++ java/com/example/bjjlearntrain/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.bjjlearntrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
